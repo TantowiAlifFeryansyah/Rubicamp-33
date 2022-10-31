06f4d0d1e3bc051b982a52cd4d6a88aafdc4bb6b
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-10-31 10:29:24 +0700

    challenge17

---
+++ challenge17/calculator.js
@@ -0,0 +1,38 @@
+/*
+buatlah class yang berfungsi sebagai mesin hitung, memiliki kemampuan seperti di bawah ini:
+
+    1. initial number
+    2. penambahan
+    3. pengurangan
+    4. pembagian
+    5. perkalian
+    6. akar pangkat 2
+    7. exponent
+    8. dapat menghitung keliling dan luas lingkaran
+*/
+
+const PI = 22 / 7
+
+class Calculator {
+    constructor() {
+
+    }
+
+    add(value) {
+
+    }
+
+    substract(value) {
+
+    }
+
+    devide(value) {
+
+    }
+
+    // tambahkan method lain yang perlu
+
+    result() {
+        
+    }
+}
