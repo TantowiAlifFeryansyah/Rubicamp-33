d6defbcb2a46789cdb61e18c7087e2bad1ba78fc
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-10-31 21:05:07 +0700

    challenge17 done

--- challenge17/calculator.js
+++ challenge17/calculator.js
@@ -11,28 +11,62 @@ buatlah class yang berfungsi sebagai mesin hitung, memiliki kemampuan seperti di
     8. dapat menghitung keliling dan luas lingkaran
 */
 
-const PI = 22 / 7
+/*
+gunakan import export ES6 untuk menggunakan 2 file javascript.
 
-class Calculator {
+KEYWORD
+method chaining, class, import, export, ES6
+*/
+
+export const PI = 22 / 7
+
+export default class Calculator {
     constructor() {
-
+        this.number = 1;
     }
 
     add(value) {
-
+        this.number = this.number + value;
+        return this;
     }
 
     substract(value) {
-
+        this.number = this.number - value;
+        return this;
     }
 
     devide(value) {
-
+        this.number = this.number / value;
+        return this;
     }
 
     // tambahkan method lain yang perlu
 
+    multiply(value) {
+        this.number = this.number * value;
+        return this;
+    }
+
+    square(value){
+        this.number = 7 ** 2;
+        return this;
+    }
+
+    exponent(value){
+        this.number = this.number ** value;
+        return this;
+    }
+
+    squareRoot(value){
+        this.number = Math.sqrt(this.number)
+        return this;
+    }
+
     result() {
-        
+        console.log(this.number);
     }
 }
+
+
+// export default Calculator
+// export default const PI = 22 / 7
---
+++ challenge17/run.js
@@ -0,0 +1,14 @@
+import Calculator, {PI} from "./calculator.js";
+
+const calc = new Calculator()
+
+calc.add(10).substract(5).result() //1 + 10 - 5 = 6
+calc.add(3).multiply(4).devide(6).result() // 6 + 3 * 4 / 6 = 6
+calc.number = 7 // set jari jari 7
+console.log(`nilai sekarang : ${calc.number}`);
+calc.multiply(2).multiply(PI).result() // keliling lingkaran dengan jari jari 7 => 2 x Pi x r = 44
+calc.x = 7 // set jari jari 7
+calc.square().multiply(PI).result() // luas lingkaran dengan jari jari 7 => PI x r pangkat 2 = 154
+calc.number = 4
+calc.exponent(3).result() // 4 pangkat 3 = 64
+calc.squareRoot().result() // akar pangkat 2 dari 64 = 8
